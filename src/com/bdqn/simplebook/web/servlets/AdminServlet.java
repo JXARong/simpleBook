fd297b61bd6b36df2ea83e42fb6f59f2a39593d7
lairong <17779799124@163.com>
2019-11-27 12:07:11 +0800

    lairong

--- src/com/bdqn/simplebook/web/servlets/AdminServlet.java
+++ src/com/bdqn/simplebook/web/servlets/AdminServlet.java
@@ -6,8 +6,6 @@ import com.bdqn.simplebook.service.AdminService;
 import com.bdqn.simplebook.service.impl.AdminServiceImpl;
 import com.bdqn.simplebook.utils.AjaxUtils;
 import com.bdqn.simplebook.utils.CodeUtils;
-import javafx.scene.control.Alert;
-import javafx.scene.layout.TilePane;
 import org.omg.CORBA.ARG_OUT;
 
 import javax.imageio.ImageIO;
